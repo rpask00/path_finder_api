d770d0affae913bd165f17c10aa14868031f4227
Rafal Pasko <rp.261000@gmail.com>
2023-06-15 22:43:25 +0200

    generate distance matrix

---
+++ distance_matrix.py
@@ -0,0 +1,30 @@
+import googlemaps
+
+from autocomplete import API_KEY
+
+gmaps = googlemaps.Client(key=API_KEY)
+
+
+def generate_distance_matrix(place_ids):
+    distance_matrix = []
+
+    for origin_id in place_ids:
+        row = []
+        for destination_id in place_ids:
+            if origin_id == destination_id:
+                row.append(0)
+            else:
+                result = gmaps.distance_matrix(origins='place_id:' + origin_id,
+                                               destinations='place_id:' + destination_id,
+                                               mode='driving')
+                distance = result['rows'][0]['elements'][0]['distance']['value']  # Get distance in meters
+                row.append(distance)
+        distance_matrix.append(row)
+
+    for row in distance_matrix:
+        print(row)
+
+
+if __name__ == '__main__':
+    _place_ids = ['ChIJd8BlQ2BZwokRAFUEcm_qrcA', 'ChIJE9on3F3HwoAR9AhGJW_fL-I']
+    generate_distance_matrix(_place_ids)
